a298df60760107ae6b90e0eeebcd8199abb4f6bb
Alejandro Molinello S <103202016+amolinello@users.noreply.github.com>
2023-02-04 07:47:42 -0500

    Update divisiónEnteros.py

--- divisiónEnteros.py
+++ divisiónEnteros.py
@@ -1,10 +1,11 @@
 # Funcion para dividir dos numeros enteros
 def dividir_dos_enteros(x, y):
     try:
+        # Retorna el resultado de la división si no tiene errores
         return x/y
+    
+    # Retorna None, muestra el mensaje con el error y el tipo de error que se da (si es el caso).
     except ZeroDivisionError as ze:
-        print('No se permite la división por cero: ', ze)
+        print(f'No se permite la división por cero: {ze}\n{type(ze)}')
     except Exception as ex:
-        print('Los datos ingresados no son validos: ', ex)
-
-print(dividir_dos_enteros(11,4))
+        print(f'Los datos ingresados no son validos: {ex}\n{type(ex)}')
